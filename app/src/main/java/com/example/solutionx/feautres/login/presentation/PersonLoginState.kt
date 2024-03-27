9f92424a208ce8004d78d3907f1a2f2183f7c0fc
heshamabdla <heshamabdla12@gmail.com>
2024-03-27 16:58:37 +0200

    .........

--- app/src/main/java/com/example/solutionx/feautres/login/presentation/PersonLoginState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.solutionx.feautres.login.presentation
-
-
-//sealed class PersonLoginState(
-//){
-//    data class IsLoading(val loading: Boolean):PersonLoginState()
-//    data class Success(val person: Person?):PersonLoginState()
-//    data class Error(val error: String?):PersonLoginState()
-//}
